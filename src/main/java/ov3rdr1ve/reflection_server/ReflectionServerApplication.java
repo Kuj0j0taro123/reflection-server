3de3a2da9825b24ab0f9d36bb6730f18c1a6dbb9
OV3RDR1VE <coredumpedsegmentationerror@protonmail.com>
2025-04-24 22:11:30 +0300

    first commit

---
+++ src/main/java/ov3rdr1ve/reflection_server/ReflectionServerApplication.java
@@ -0,0 +1,13 @@
+package ov3rdr1ve.reflection_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ov3rdr1ve/reflection_server/ReflectionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ov3rdr1ve.reflection_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
